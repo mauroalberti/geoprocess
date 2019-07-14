ba472bfb9a23c01b0e2d851cbafdfc7686f0b635
mauroalberti <alberti.m65@gmail.com>
2019-07-14 17:21:12 +0200

    Started structural station

---
+++ geoprocess/geology/base.py
@@ -0,0 +1,19 @@
+
+class StructuralStation:
+
+    def __init__(self,
+            location: Point,
+            stratifications: Optional[List[Plane]],
+            foliations: Optional[List[Plane]],
+            faults: Optional[List[Fault]]):
+        """
+
+        :param location:
+                :param location:
+        :param stratifications:
+        :param stratifications:
+        :param foliations:
+        :param foliations:
+        :param faults:
+        :param faults:
+        """
